457effe119a0864f04bdd94c1b1751e1761f5583
ohigusu <suyk52@gmail.com>
2024-08-12 16:55:21 +0900

    Time: 62 ms (97.6%), Space: 18.1 MB (78.54%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -2,22 +2,23 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         if graph[0] == []: return []
         
-        N,path,ret_path = len(graph),{},[]
+        N,path,result = len(graph),{},[]
         
         for i, g in enumerate(graph):path[i] = g
         
         stack = [[path[0], [0]]]
+
         while stack:
             p, path_history = stack.pop()
-
             for each_path in p:
                 if each_path == N-1:
-                    ret_path.append(path_history + [each_path])
-                    continue
+                    result.append(path_history + [each_path])
+
                 else:
                     path_history.append(each_path)
                     for next_path in path[each_path]:
                             stack.append([[next_path], path_history[:]])
                     path_history.pop()
-        return ret_path
+
+        return result
 
