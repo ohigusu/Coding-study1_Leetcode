a7f2c07ee8874af20d6fa021aa2bbd1f515b0525
ohigusu <suyk52@gmail.com>
2024-08-12 15:25:32 +0900

    Time: 59 ms (99.24%), Space: 18.3 MB (41.31%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -5,9 +5,9 @@ class Solution:
         def dfs(graph,node,path,n):
             path.append(node)
             if node == n : result.append(path[:])
-            for neigh in graph[node]:
-                dfs(graph,neigh,path,n)
-                path.pop()
-            
+            else:
+                for neigh in graph[node]:
+                    dfs(graph,neigh,path,n)  
+            path.pop()
             return result
         return dfs(graph,0,[],len(graph)-1)
