8c2cce71f7e21dacf1634631c0f4034c2df01b38
ohigusu <suyk52@gmail.com>
2024-08-12 16:50:28 +0900

    Time: 74 ms (54.57%), Space: 18 MB (78.54%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,13 +1,27 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        result = []
-         
-        def dfs(graph,node,path,n):
-            path.append(node)
-            if node == n : result.append(path[:])
-            else:
-                for neigh in graph[node]:
-                    dfs(graph,neigh,path,n)  
-            path.pop()
-            return result
-        return dfs(graph,0,[],len(graph)-1)
+        if graph[0] == []:
+            return []
+        
+        N = len(graph)
+        path = {}
+        for i, g in enumerate(graph):
+            path[i] = g
+        
+        stack = [[path[0], [0]]]
+        ret_path = []
+        while stack:
+            # print 'stack =',stack
+            p, path_history = stack.pop()
+            for each_path in p:
+                if each_path == N-1:
+                    ret_path.append(path_history + [each_path])
+                    continue
+                if each_path not in path_history:
+                    path_history.append(each_path)
+                    for next_path in path[each_path]:
+                        if next_path not in path_history:
+                            stack.append([[next_path], path_history[:]])
+                    path_history.pop()
+        return ret_path
+
