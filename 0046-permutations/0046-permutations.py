8e3dae41d6d1d416c764598cdd18489c83336c65
ohigusu <suyk52@gmail.com>
2024-08-21 17:36:18 +0900

    Update 0046-permutations.py

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return []
-        if len(nums) == 1:
-            return [nums]
+        if len(nums) == 0: return []
+        if len(nums) == 1: return [nums]
         result = []
+        #부모노드
         for i in range(len(nums)):
             current = nums[i]
             remaining = nums[:i]+nums[i+1:]
-
+            #모든 자식노드
             for p in self.permute(remaining):
                 result.append([current]+p)
         return result
