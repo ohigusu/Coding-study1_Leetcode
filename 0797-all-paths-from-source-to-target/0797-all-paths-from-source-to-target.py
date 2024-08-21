fec7c60b19b7e50b1129f1fde9f7de9eab665f12
ohigusu <suyk52@gmail.com>
2024-08-21 19:36:12 +0900

    Time: 76 ms (39.55%), Space: 18.2 MB (44.74%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,26 +1,22 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        if graph[0] == []: return []
-        
-        N,path,result = len(graph),{},[]
-        
-        for i, g in enumerate(graph):path[i] = g
-        
-        queue = deque([[path[0], [0]]])
+        answer = []
+        for i in range(len(graph)-1):
+            if graph[i] == []:
+                last = i
+        def backtracking(graph,node,visted):
+            #해결 조건과 일치
+            if node == len(graph)-1:
+                answer.append([0]+visted[:])
+            else:
+                for neighbor in graph[node]:
+                    #후보군
+                    if neighbor not in visted:
+                        visted.append(neighbor)
+                        backtracking(graph,neighbor,visted)
+                        visted.pop()
+        backtracking(graph,0,[])
+        return answer
+                
 
-        while queue:
-            p, path_history = queue.pop()
-            
-            for node in p:
-                if node == N-1:
-
-                    result.append(path_history + [node])
-
-                else:
-                    path_history.append(node)
-                    for next_path in path[node]:
-                            queue.append([[next_path], path_history[:]])
-                    path_history.pop()
-
-        return result
 
