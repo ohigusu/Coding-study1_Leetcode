aa2202a9046ca2c81416cf06b790a0b04abe6669
ohigusu <suyk52@gmail.com>
2024-08-27 01:11:41 +0900

    Create sol2.py

---
+++ 0784-letter-case-permutation/sol2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def letterCasePermutation(self, S: str) -> List[str]:
+        answer = [""]
+        for i in range(len(S)):
+            temp = []
+            for j in range(len(answer)):
+                if S[i].isalpha():
+                    temp.append(answer[j] + S[i].lower())
+                    temp.append(answer[j] + S[i].upper())
+                else:
+                    temp.append(answer[j] + S[i])
+            answer = temp
+        return answer
