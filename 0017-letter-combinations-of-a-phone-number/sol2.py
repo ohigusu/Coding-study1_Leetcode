28a3744335be64493dfa23658b316d50dd293d33
ohigusu <suyk52@gmail.com>
2024-09-02 18:42:41 +0900

    Create sol2.py

---
+++ 0017-letter-combinations-of-a-phone-number/sol2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":return []
+        letters = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        answer,dict = [],[]
+        for i in digits:
+            dict.append(letters[i])
+        def dfs(visited,idx):
+            if len(visited) == len(dict):
+                answer.append(visited)
+            else:
+                for char in dict[idx]:
+                    dfs(visited+char,idx+1)
+        dfs("",0)
+        return answer
