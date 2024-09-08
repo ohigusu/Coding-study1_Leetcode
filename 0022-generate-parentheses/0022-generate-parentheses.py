478812f50c58922a69a4e88e0a549e9ba3a9be24
ohigusu <suyk52@gmail.com>
2024-09-08 17:29:11 +0900

    Time: 33 ms (83.15%), Space: 16.8 MB (51.35%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,9 +2,8 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         answer = []
         def back(start,end,visited):
-            if (end == n): 
-                answer.append(visited[:])
-                return 
+            if end == n: 
+                answer.append(visited)
             if start >= end+1:
                 back(start,end+1,visited+")")
             if start < n:
