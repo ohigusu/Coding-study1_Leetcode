a1559600605bda285755040eec8280b84aab7e1d
ohigusu <suyk52@gmail.com>
2024-09-09 10:20:41 +0900

    Update 0078-subsets.py

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         answer = []
-        def fun(visited,idx):
+        def backtrack(visited,idx):
             answer.append(visited[:])
-            for i in range(idx,len(sort_num)):
-                if sort_num[i] not in visited:
-                    visited.append(sort_num[i])
-                    fun(visited,i+1)
+            for i in range(idx,len(nums)):
+                if nums[i] not in visited:
+                    visited.append(nums[i])
+                    backtrack(visited,i+1)
                     visited.pop()
-        fun([],0)
+        backtrack([],0)
         return answer
-                
