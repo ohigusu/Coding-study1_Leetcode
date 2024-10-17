e9778cb9f2b0a1fb9a56f0899f118de2fe4e0df8
ohigusu <suyk52@gmail.com>
2024-10-17 10:49:48 +0900

    Time: 39 ms (70.63%), Space: 17.5 MB (56.94%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -8,12 +8,15 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if root is None: return False
         def traverse(root,currSum):
+            #leaf node
             if not root.left and not root.right:
                 return currSum == targetSum
+            #left subtree
             if root.left and traverse(root.left,currSum+root.left.val):
                 return True
+            #right subtree
             if root.right and traverse(root.right,currSum+root.right.val):
-                return True
+                return True 
             return False
         return traverse(root,root.val)
                     
