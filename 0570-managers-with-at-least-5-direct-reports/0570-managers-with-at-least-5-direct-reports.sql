fceaf0a2622ebfe6063eb6faca3f3d88179aacf0
ohigusu <suyk52@gmail.com>
2024-10-24 23:26:35 +0900

    Time: 364 ms (38.56%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT name
+FROM Employee
+WHERE id  IN (SELECT (CASE WHEN COUNT(DISTINCT id) >= 5 THEN managerId END) AS managerId 
+                        FROM Employee
+                        GROUP BY managerId )
+    
