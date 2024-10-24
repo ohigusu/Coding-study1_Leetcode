7541adb93da58dd7af15c09dc42a2d28913b4883
ohigusu <suyk52@gmail.com>
2024-10-24 23:29:15 +0900

    Update sol2.sql

--- 0570-managers-with-at-least-5-direct-reports/sol2.sql
+++ 0570-managers-with-at-least-5-direct-reports/sol2.sql
@@ -3,3 +3,12 @@ FROM Employee
 WHERE id  IN (SELECT (CASE WHEN COUNT(DISTINCT id) >= 5 THEN managerId END) AS managerId 
                         FROM Employee
                         GROUP BY managerId )
+
+#version2
+SELECT name  
+FROM EMPLOYEE 
+WHERE id IN (SELECT managerId
+              FROM EMPLOYEE
+              GROUP BY ManagerId
+                HAVing COUNT(*)>=5
+)
