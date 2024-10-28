d210143e8c5c90796f50c3c9dd6a2ff1ce77eadd
ohigusu <suyk52@gmail.com>
2024-10-28 10:18:27 +0900

    Time: 549 ms (58.46%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
 SELECT player_id
-      ,DATE_FORMAT(MIN(event_date),"%Y-%m-%d") AS first_login 
+      ,DATE_FORMAT(event_date,'%Y-%m-%d') AS first_login
+FROM(
+SELECT player_id
+      ,event_date
+      ,ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS login_date
 FROM Activity
-GROUP BY player_id
+) AS logintab
+WHERE login_date = 1
+
